de91b480f76f670fa656b87aa993707c350f8576
matyvan <77383369+matyvan3@users.noreply.github.com>
2022-08-12 23:21:27 +0200

    Sorter V2.1
    Přidáno ovládání servo motorů, focení karty a čtení signálu z ovládacího panelu

--- SorterEngine.py
+++ SorterEngine.py
@@ -1,22 +1,25 @@
 from os import system as run
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 from time import sleep as wait
 from difflib import SequenceMatcher
+from picamera import piCamera
 import pytesseract as OCR
+import pigpio
 
 #set up Card class with details of the current card
 class Card:
     def __init__(self, picture_path):
-        self.image = Image.open(picture_path)
+        self.image = Image.open(picture_path).crop((250, 320, 2960, 1630))
         self.name = ""
         self.valid = False
         self.detect()
 
     #prepares image to be readable by OCR
     def prepare_image(self):
-        for x in range(5):
-            self.image = self.image.filter(ImageFilter.SHARPEN)
-        self.image = self.image.filter(ImageFilter.GaussianBlur)
+        self.image = ImageEnhance.Brightness(self.image).enhance(1.5)
+        self.image = ImageEnhance.Color(self.image).enhance(0)
+        self.image = ImageEnhance.Sharpness(self.image).enhance(2)
+        self.image = ImageEnhance.Contrast(self.image).enhance(4)
 
     #takes text read from card and compares it with names in database to find the most probable card
     def match_card(self, reading):
@@ -27,8 +30,8 @@ class Card:
             similarity = round(matcher.ratio(), 5)
             if similarity > same[1]:
                 same = [name, similarity]
-        print(reading, same)
         self.name = same[0]
+        self.similarity = same[1]
         
     #card detection method - detects text and fetches card's values
     def detect(self):
@@ -44,7 +47,12 @@ class Card:
             self.match_card(self.name)
             self.properties = properties[names.index(self.name)]
             self.valid = True
-           
+
+#preheat camera
+cam = PiCamera()
+cam.resolution = (3280, 2464)
+cam.start_preview()
+
 #prepare card database
 names, properties = [], []
 with open("CardsList.txt", "r", encoding = "utf8") as CardsList:
@@ -54,56 +62,106 @@ with open("CardsList.txt", "r", encoding = "utf8") as CardsList:
         properties.append(line[1].split("|"))
         properties[-1][-1] = properties[-1][-1][:-1].split(";")
 
-#get going
+#prepare GPIO
+Pin = pigpio.pi()
+Pin_enable = 6
+X_step = 26
+X_dir = 21
+Y_step = 19
+Y_dir = 20
+Pin_col = 22
+Pin_val = 27
+Pin_ed = 17
+Pin_reset = 14
+Pin_start = 2
+
+for pin in [Pin_ed, Pin_col, Pin_val, Pin_reset, Pin_start]:
+    Pin.set_mode(pin, pigpio.INPUT)
+for pin in [Pin_enable, X_step, Y_step, X_dir, Y_dir]:
+    Pin.set_mode(pin, pigpio.OUTPUT)
+
+#define movement methods
+def move_x(direction, distance):
+    Pin.write(X_dir, direction)
+    for temp in range(distance):
+        Pin.write(X_step, 1)
+        wait(0.042)
+        Pin.write(X_step, 0)
+        wait(0.042)
+        
+def move_y(direction, distance):
+    Pin.write(Y_dir, direction)
+    for temp in range(distance):
+        Pin.write(Y_step, 1)
+        wait(0.042)
+        Pin.write(Y_step, 0)
+        wait(0.042)
+
+def pos_reset():
+    Pin.write(Pin_enable, 0)
+    while not Pin_reset.read():
+        move_x(0, 16)
+    Pin.write(Pin_enable, 1)
+
+#last variables' initiation
 end = False
 stacks = []
-props = input("Properties?(0 = color, 1 = mana value, 2 = edition): ")
-while not end:
-        #take the card picture and prepare it for detection
-        picture_path = input("name: ").replace(" ", "_").lower() + ".jpg"
-        card = Card(picture_path)
-        if card.valid:
-            #grab the user-chosen categories to compare
-            categories = []
-            for cat in map(int, props):
-                categories.append(card.properties[cat])
-            if "2" in props:
-                editions = True
-            else:
-                editions = False
-                
-            #find the stack to drop card on
-            stacknum = 0
-            not_this = True
-            if len(stacks) > 0:
-                for stack in stacks:
-                    not_this = False
-                    for x in range(len(categories) - editions):
-                        if stack[x] == card.properties[x]:
-                            pass
-                        else:
-                            not_this = True
-                    if editions:
-                        edition_intersect = [edition for edition in card.properties[-1] if edition in stack[-1]]
-                        if len(edition_intersect) > 0:
-                            stack[-1] = edition_intersect
-                        else:
-                            not_this = True
-                    if not_this and stacknum < 24:
-                        stacknum += 1
-                    else:
-                        break
-                
-            #remember new stack
-            if len(stacks) < 25 and not_this:
-                stacks.append(categories)
-            elif len(stacks) == 25 and not_this:
-                stacks.append("leftover")
-                
-            #find out the position to go to and drop off the card
-            position = stacknum
+stacklength = 6950 #(distance to cover / distance per revolution) * steps per revolution
+rnat = 0
 
-        #in case card is not readable enough, drop it in the leftover bin
-        else:
-            position = 25
-        print(position, stacks[position])
+#wait for button press
+while not Pin.read(Pin_start):
+    pass
+wait(1)
+
+#loop until last card or second button press
+while not end and not Pin.read(Pin_start):
+    
+    #take the card picture and prepare it for detection
+    picture_path = "/home/pi/Desktop/card.jpg"
+    cam.capture(picture_path)
+    card = Card(picture_path)
+
+    #if valid, find its stack
+    if card.valid:
+        stacknum = 0
+        not_this = True
+        if len(stacks) > 0:
+            for stack in stacks:
+                not_this = False
+                if Pin.read(Pin_col):
+                    if stack[0] == card.properties[0]:
+                        pass
+                    else:
+                        not_this = True
+                if Pin.read(Pin_val):
+                    if stack[1] == card.properties[1]:
+                        pass
+                    else:
+                        not_this = True
+                if Pin.read(Pin_ed):
+                    edition_intersect = [edition for edition in card.properties[-1] if edition in stack[-1]]
+                    if len(edition_intersect) > 0:
+                        stack[-1] = edition_intersect
+                    else:
+                        not_this = True
+                if not_this and stacknum < 24:
+                    stacknum += 1
+                else:
+                    break
+                
+        #remember new stack
+        if len(stacks) < 25 and not_this:
+            stacks.append(card.properties)
+        elif len(stacks) == 25 and not_this:
+            stacks.append("leftover")
+
+    #in case card is not readable enough, leftover stack it is
+    else:
+        stacknum = 25
+        
+    #go to the correct position and drop off the card
+    Pin.write(Pin_enable, 0)
+    move_x(stacknum>rnat, abs(stacknum-rnat)*stacklength)
+    move_y(stacknum%2, 256)
+    Pin.write(Pin_enable, 1)
